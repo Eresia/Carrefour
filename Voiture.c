d309c00fcccb3c4f08e3243853d0595fb8aab1b2
Eresia <bastien.lepesant@gmail.com>
2016-04-04 15:20:06 +0200

    Voiture 1

--- Voiture.c
+++ Voiture.c
@@ -1,1 +1,27 @@
 #include "Voiture.h"
+
+Voiture* start_voiture(int id, int voie, key_t keyFeu, key_t keySemVoie, key_t keySemCar){
+
+	if(fork() == 0){
+		Voiture* v = malloc(sizeof(Voiture));
+		int* feu = shmalloc(keyFeu, sizeof(int));
+		int* semVoie = (int*) shmalloc(keySemVoie, sizeof(int));
+		int* semCar = (int*) shmalloc(keySemVoie, sizeof(int));
+		v->feu = feu;
+		v->semVoie = semVoie;
+		v->semCar = semCar;
+		v->id = id;
+		v->voie = voie;
+
+		return v;
+	}
+	else{
+		return NULL;
+	}
+
+}
+
+void enterCarrefour(Voiture* voiture){
+	P(*voiture->semVoie);
+	printf("VOITURE : arrivee de la voiture %d sur la voie %d", voiture->id, voiture->voie);
+}
--- Voiture.h
+++ Voiture.h
@@ -1,4 +1,22 @@
 #ifndef VOITURE
 #define VOITURE
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "ipcTools.h"
+
+enum bool {false = 0, true = 1};
+
+typedef struct Voiture Voiture;
+struct Voiture{
+		int id;
+		int* semVoie;
+		int* semCar;
+		int* feu;
+		int voie;
+};
+
+Voiture* start_voiture(int id, int voie, key_t feu, key_t keySemVoie, key_t keySemCar);
+void enterCarrefour(Voiture* voiture);
 #endif

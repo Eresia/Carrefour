3015e1b3cf8f5d1bec09bff68a2e64911ba5561b
Eresia <bastien.lepesant@gmail.com>
2016-04-04 14:13:43 +0200

    Ajout des ipctools

---
+++ ipcTools.c
@@ -0,0 +1,79 @@
+#include "ipcTools.h"
+
+static struct sembuf sbuf;
+
+int semalloc(key_t key, int vallInit){
+	int semid;
+	semid = semget(key, 1, IPC_CREAT|IPC_EXCL|0600);
+	if(semid != -1){
+		if(semctl(semid, 0, SETVAL, vallInit) != -1){
+			return semid;
+		}
+	}
+	return semid;
+}
+
+int mutalloc(key_t key){
+	int semid;
+	semid = semget(key, 1, IPC_CREAT|IPC_EXCL|0600);
+	if(semid != -1){
+		if(semctl(semid, 0, SETVAL, 1) != -1){
+			return semid;
+		}
+	}
+	return semid;
+}
+
+void *shmalloc(key_t key, int size){
+	int shmid = shmget(key, size, IPC_CREAT|IPC_EXCL|0600);
+	if(shmid == -1){
+		shmid = shmget(key, size, 0600);
+	}
+	if(shmid != -1){
+		void *addr = shmat(shmid, 0, 0);
+		if(addr == -1){
+			shmctl(shmid, IPC_RMID, 0);
+		}
+		else{
+			return addr;
+		}
+	}
+	return 0;
+}
+
+int shmfree(key_t key, void *shmaddr){
+	int shmid = shmget(key, 0, 0600);
+	if(shmid != -1){
+		shmdt(shmaddr);
+		shmctl(shmid, IPC_RMID, 0);
+		return 0;
+	}
+	return -1;
+}
+
+void P(int semid){
+		sbuf.sem_op = -1;
+		semop(semid, &sbuf, 1);
+}
+
+void V(int semid){
+		sbuf.sem_op = +1;
+		semop(semid, &sbuf, 1);
+}
+
+int semfree(int semid){
+	return semctl(semid, 0, IPC_RMID, 0);
+}
+
+int main(){
+	key_t ipcKey = ftok("/home/utilisateur/Bureau/OS/td4", 0);
+	key_t shmkey = ipckey+1;
+	int semid = mutalloc(ipcKey);
+	printf("Creation : %d\n", semid);
+		printf("Id : %d libere\n", semid);
+		if(semfree(semid) == 0){
+	}
+	else{
+		printf("Id : %d déja libre\n", semid);
+	}
+}
---
+++ ipcTools.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+int semalloc(key_t key, int vallInit);
+int mutalloc(key_t key);
+
+void P(int semid);
+void V(int semid);
+
+void *shmalloc(key_t key, int size);
+
+int shmfree(key_t key, void *shmaddr);
+
+int semfree(int semid);
